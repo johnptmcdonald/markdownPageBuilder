3e29e899c4b95765d3089fe5df7f1ba099d1c50c
johnptmcdonald <johnptmcdonald@gmail.com>
2020-04-22 18:25:14 -0400

    chore(layout): adds rebass to layout

--- webapp/src/components/layout.js
+++ webapp/src/components/layout.js
@@ -8,8 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import { Box, Header, Button } from "rebass"
+// import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,21 +25,14 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <Box m={[3, 4, 5]} mt={[2, 3, 4]}>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </Box>
     </>
   )
 }
